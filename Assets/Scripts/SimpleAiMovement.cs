6a53a6998aebc6fdf98972c9f578d70bfe5982bf
Quinn L <larson.quinn@gmail.com>
2021-09-18 16:44:10 -0700

    Add basic pathfinding. Boss moves to player

---
+++ Assets/Scripts/SimpleAiMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+
+public class SimpleAiMovement : NetworkBehaviour
+{
+    NavMeshAgent agent;
+    //[SerializeField] Transform target;
+    Transform target;
+
+    bool swapper = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        FindPlayer();
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isServer)
+            return;
+
+        UpdateDestination();    
+    }
+
+    void UpdateDestination()
+    {
+        Vector3 add = new Vector3(.001f, 0);
+        if (swapper)
+        {
+            swapper = !swapper;
+            agent.SetDestination(target.position + add);
+        }
+        else
+        {
+            swapper = !swapper;
+            agent.SetDestination(target.position - add);
+        }
+    }
+    void FindPlayer()
+    {
+        if (!isServer)
+            return;
+
+        GameObject[] gamers =  GameObject.FindGameObjectsWithTag("Player");
+        target = gamers[0].GetComponent<Transform>();
+
+    }
+}
