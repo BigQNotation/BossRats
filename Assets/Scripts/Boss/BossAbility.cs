099bf6d7707314e82309caaaa8de2d15850a164b
Quinn L <larson.quinn@gmail.com>
2021-09-25 11:26:21 -0700

    Finish BossAbilityConfigGenerator

---
+++ Assets/Scripts/Boss/BossAbility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BossAbility : NetworkBehaviour
+{
+    protected int abilityID { get; set; }
+    protected float COOLDOWN { get; set; }
+
+    public float cooldownTimer;
+
+
+    void Start()
+    {
+        ResetCooldown();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public virtual void UseAbility()
+    {
+
+    }
+
+    public void ResetCooldown()
+    {
+        cooldownTimer = COOLDOWN;
+    }
+
+    public bool AbilityReady()
+    {
+        if (cooldownTimer <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public int GetAbilityID()
+    {
+        return abilityID;
+    }
+    protected void DecrementCooldown()
+    {
+        cooldownTimer = Mathf.Max(cooldownTimer - Time.deltaTime, 0f);
+    }
+
+}
---
+++ Assets/Scripts/Boss/BossAbilityConfigGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BossAbilityConfigGenerator : NetworkBehaviour
+{
+    private BossAbility[] allAbilitiesList;
+    private BossAbility[] randomAbilityList;
+
+    public BossAbility[] GetRandomAbilityList()
+    {
+        return randomAbilityList;
+    }
+    public void GenerateRandomAbilityList(int numberOfAbilities)
+    {
+        if (numberOfAbilities > allAbilitiesList.Length || numberOfAbilities < 1)
+            throw new System.Exception("Error: Number of abilities requested is invalid. Number must be between 1 and the total implemented.");
+
+        int[] randomIndexArray = GetRandomUniqueIntArray(numberOfAbilities, allAbilitiesList.Length);
+        Debug.Log(randomIndexArray.Length);
+        randomAbilityList = new BossAbility[numberOfAbilities];
+        for (int i = 0; i < randomIndexArray.Length; i++)
+        {
+            randomAbilityList[i] = allAbilitiesList[randomIndexArray[i]];
+        }
+
+    }
+    private int[] GetRandomUniqueIntArray(int arraySize, int arrayMaxRange)
+    {
+        int[] result = new int[arraySize];
+        List<int> numbersInOrder = new List<int>();
+        for (var x = 0; x < arrayMaxRange; x++)
+        {
+            numbersInOrder.Add(x);
+        }
+        for (var x = 0; x < arraySize; x++)
+        {
+            var randomIndex = UnityEngine.Random.Range(0, numbersInOrder.Count);
+            result[x] = numbersInOrder[randomIndex];
+            numbersInOrder.RemoveAt(randomIndex);
+        }
+
+        return result;
+    }
+    private void SetAllAbilityList()
+    {
+        BossAbility[] abilityList = { gameObject.GetComponent<BossAbilityPellet>() };
+        allAbilitiesList = abilityList;
+    }
+    void Start()
+    {
+        if (!isServer)
+            return;
+
+        SetAllAbilityList();
+    }
+    void Update()
+    {
+        if (!isServer)
+            return;
+    }
+}
