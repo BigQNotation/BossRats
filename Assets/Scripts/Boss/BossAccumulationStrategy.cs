467a86134aaf9518630505e6b558b9ccfa767daf
Quinn L <larson.quinn@gmail.com>
2021-09-27 12:38:22 -0700

    Add BossModification class. Add BossAccumulationStrategy base class. Add AccumulationDamageOut class.

---
+++ Assets/Scripts/Boss/BossAccumulationStrategy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAccumulationStrategy : MonoBehaviour
+{
+    private int stateCount;
+    private int currentState = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected void SetNumberOfStates(int states)
+    {
+        stateCount = states;
+    }
+
+    public void IncreaseState()
+    {
+        if (currentState < stateCount)
+            currentState++;
+    }
+    public void ResetState()
+    {
+        currentState = 0;
+    }
+    public int GetCurrentState()
+    {
+        return currentState;
+    }
+    public bool CheckMaxState()
+    {
+        if (currentState == stateCount)
+            return true;
+        else
+            return false;
+    }
+
+    public virtual void UseStrategy()
+    {
+
+    }
+    public virtual void ResetStrategy()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Boss/Strategies/AccumulationDamageOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccumulationDamageOut : BossAccumulationStrategy
+{
+    int STATES = 3;
+    int DAMAGEINCREASE = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNumberOfStates(STATES);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void UseStrategy()
+    {
+        gameObject.GetComponent<BossModifications>().damageIncreasePercent += DAMAGEINCREASE;
+    }
+    public override void ResetStrategy()
+    {
+        gameObject.GetComponent<BossModifications>().damageIncreasePercent -= (GetCurrentState() * DAMAGEINCREASE);
+    }
+
+}
