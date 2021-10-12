f030a54b26786642e73dd720127ef9932838deb2
Quinn L <larson.quinn@gmail.com>
2021-10-12 15:05:33 -0700

    Add boss health via BossStats class. Add boss health UI, BossHealthUI class.

---
+++ Assets/BossStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BossStats : NetworkBehaviour
+{
+    [SyncVar]
+    int bossHealth = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int GetBossHealth()
+    {
+        return bossHealth;
+    }
+    public void DecrementBossHealth(int healthDec)
+    {
+        bossHealth -= healthDec;
+    }
+}
---
+++ Assets/Scripts/UI/BossHealthUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class BossHealthUI : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject boss;
+    [SerializeField]
+    GameObject healthTextObject;
+    [SyncVar]
+    string bossHealthText;
+
+    void Start()
+    {        
+    }
+    void Update()
+    {
+        if (isServer)
+            bossHealthText = GetBossHealth();
+        
+        SetBossHealthUI(bossHealthText);
+    }
+
+    private string GetBossHealth()
+    {
+        return boss.GetComponent<BossStats>().GetBossHealth().ToString();
+    }
+    private void SetBossHealthUI(string healthText)
+    {
+        healthTextObject.GetComponent<Text>().text = healthText;
+    }
+}
