fc0796f48f5ae79cee576d2ee692acfe88d4363c
Quinn L <larson.quinn@gmail.com>
2021-10-18 12:34:07 -0700

    Add GameOverSceneSwapper; When players no longer have remaining health, change scene back to lobby.

---
+++ Assets/Scripts/GameOverSceneSwapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameOverSceneSwapper : NetworkBehaviour
+{
+    [Scene]
+    public string lobbyScene;
+    GameObject[] players;
+    bool sceneAlreadySwapped = false;
+
+    private void Update()
+    {
+        if (!isServer)
+            return;
+
+        if (AreAllPlayersDead())
+            ChangeSceneToLobby();
+    }
+
+    private bool AreAllPlayersDead()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        int deadPlayerCount = 0;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].GetComponent<PlayerStats>().GetCurrentPlayerHealth() <= 0)
+                deadPlayerCount++;
+        }
+
+        if (deadPlayerCount == players.Length)
+            return true;
+        else
+            return false;
+    }
+    private void ChangeSceneToLobby()
+    {
+        if (sceneAlreadySwapped)
+            return;
+        ((NetworkRoomManagerNew)NetworkManager.singleton).ChangeScene();
+
+        sceneAlreadySwapped = true;
+    }
+}
