2a71e50e2f658530851452b7cab7ddb1536311a8
Quinn L <larson.quinn@gmail.com>
2021-10-23 16:18:10 -0700

    Add PlayerLoadStatus class; Add functionality to check if all players are loaded into the main game scene.

---
+++ Assets/Scripts/PlayerLoadHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerLoadHandler : NetworkBehaviour
+{
+    NetworkRoomManagerNew roomManager;
+    [SyncVar] private bool arePlayersLoaded = false;
+
+    public bool ArePlayersLoaded()
+    {
+        return arePlayersLoaded;
+    }
+
+    private void Start()
+    {
+        if (!isServer)
+            return;
+        roomManager = GameObject.Find("RoomManager").GetComponent<NetworkRoomManagerNew>();
+    }
+    private void Update()
+    {
+        if (!isServer)
+            return;
+        UpdatePlayerLoadStatus();
+    }
+    private void UpdatePlayerLoadStatus()
+    {
+        if (roomManager.roomSlots.Count == GameObject.FindGameObjectsWithTag("Player").Length)
+            arePlayersLoaded = true;
+    }
+}
