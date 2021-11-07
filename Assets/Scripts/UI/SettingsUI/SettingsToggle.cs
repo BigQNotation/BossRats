e93ed89b650351ef00763f7936c1ec643f5739da
Quinn L <larson.quinn@gmail.com>
2021-11-06 17:09:37 -0700

    Add settings interface to offline scene.

---
+++ Assets/Scripts/UI/SettingsUI/SettingsToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsToggle : MonoBehaviour
+{
+    [SerializeField] GameObject settingsPrefab;
+    GameObject settings;
+    public static bool isSettingActive = false;
+
+    public void ToggleSettings()
+    {
+        if (!isSettingActive)
+            CreateSettingsInterface();
+        else
+            DestroySettingsInterface();
+        isSettingActive = !isSettingActive;
+    }
+    private void CreateSettingsInterface()
+    {
+        settings = Instantiate(settingsPrefab);
+        settings.GetComponentInChildren<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+    private void DestroySettingsInterface()
+    {
+        Destroy(settings);
+    }
+}
