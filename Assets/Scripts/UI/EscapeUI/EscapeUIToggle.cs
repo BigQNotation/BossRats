bffc2818fea9c9b0c3b9ce0aa335440dc73c151c
Quinn L <larson.quinn@gmail.com>
2021-11-10 14:54:51 -0800

    Add escape interface to offline scene.

---
+++ Assets/Scripts/UI/EscapeUI/EscapeUIToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeUIToggle : MonoBehaviour
+{
+    [SerializeField] InterfaceHandler interfaceHandler;
+    [SerializeField] GameObject escapeInterfacePrefab;
+    private GameObject escapeInterface;
+    private bool isInterfaceActive = false;
+
+    private void Update()
+    {
+        TryToggleEscapeUI();
+    }
+    private void TryToggleEscapeUI()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isInterfaceActive)
+            EnableEscapeUI();
+
+        else if (Input.GetKeyDown(KeyCode.Escape) && isInterfaceActive)
+            DisableEscapeUI();
+    }
+    private void EnableEscapeUI()
+    {
+        escapeInterface = interfaceHandler.AddInterfaceByPrefab(escapeInterfacePrefab);
+        isInterfaceActive = !isInterfaceActive;
+    }
+    private void DisableEscapeUI()
+    {
+        interfaceHandler.RemoveActiveInterface();
+        Destroy(escapeInterface);
+        isInterfaceActive = !isInterfaceActive;
+    }
+}
