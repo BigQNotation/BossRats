e10d9a42809a262c9184825de593a95776f6e9bf
Quinn L <larson.quinn@gmail.com>
2021-11-26 18:49:12 -0800

    Add ability to spectate players while dead.

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,10 +5,30 @@ using Mirror;
 
 public class PlayerCamera : NetworkBehaviour
 {
-    void Update()
+    private Transform playerTransform;
+
+    public void SetPlayerToSpectate(Transform player)
+    {
+        playerTransform = player;
+    }
+    public void ResetPlayerToSpectate()
+    {
+        playerTransform = transform;
+    }
+
+    private void Start()
+    {
+        playerTransform = transform;
+    }
+    private void Update()
     {
         if (!isLocalPlayer) return;
-        Camera.main.transform.position = transform.position + (new Vector3(0, 0, -1));
-        Camera.main.transform.LookAt(transform);
+
+        ViewPlayer();
+    }
+    private void ViewPlayer()
+    {
+        Camera.main.transform.position = playerTransform.position + (new Vector3(0, 0, -1));
+        Camera.main.transform.LookAt(playerTransform);
     }
 }
---
+++ Assets/Scripts/Player/PlayerSpectate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSpectate : NetworkBehaviour
+{
+    PlayerCamera playerCamera;
+    PlayerLoadHandler playerLoadHandler;
+    PlayerStats playerStats;
+    GameObject[] players;
+    int currentSpectatedPlayer = 0;
+
+    private void Start()
+    {
+        playerLoadHandler = GameObject.Find("PlayerLoadHandler").GetComponent<PlayerLoadHandler>();
+        playerCamera = gameObject.GetComponent<PlayerCamera>();
+        playerStats = gameObject.GetComponent<PlayerStats>();
+    }
+
+    private void Update() {
+
+        if (!PlayerCanSpectate())
+            return;
+
+        GetPlayerObjects(); // do once
+        UpdateSpectatedPlayer();
+    }
+    private bool PlayerCanSpectate()
+    {
+        if (!isLocalPlayer || !playerLoadHandler.ArePlayersLoaded() || (playerStats.GetCurrentPlayerHealth() > 0))
+            return false;
+        return true;
+    }
+    private void GetPlayerObjects()
+    {
+        if (players == null)
+            players = GameObject.FindGameObjectsWithTag("Player");
+    }
+    private void UpdateSpectatedPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (currentSpectatedPlayer == players.Length - 1)
+                currentSpectatedPlayer = 0;
+            else
+                currentSpectatedPlayer++;
+
+            playerCamera.SetPlayerToSpectate(players[currentSpectatedPlayer].transform);
+        }
+    }
+}
+
