e12f69f4c08946dd87ba1470d07903390c01a6e4
Quinn L <larson.quinn@gmail.com>
2021-12-07 17:05:11 -0800

    Remove back to room button from game scene for clients.

---
+++ Assets/Scripts/InterfaceOrderDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceOrderDelay : MonoBehaviour
+{
+    InterfaceHandler interfaceHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interfaceHandler = GameObject.Find("HANDLERS").GetComponent<InterfaceHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interfaceHandler.GetNumberOfActiveInterfaces() == 2 && interfaceHandler.IsInterfaceActive(gameObject))
+            interfaceHandler.SwapInterfaces(0, 1);
+    }
+}
